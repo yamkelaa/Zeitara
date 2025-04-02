02925f94eb909e229e62039ef1dcb9c45f49b664
yamkelaa <yamkela.qhogwana@outlook.com>
2025-04-02 23:56:01 +0200

    Commit

--- zeitara_webapp/src/app/app.component.ts
+++ zeitara_webapp/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [ButtonModule]
 })
 export class AppComponent {
   title = 'zeitara_webapp';
