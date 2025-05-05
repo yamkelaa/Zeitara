374c252e853a2e66ccbd285f66bcae6854e38d14
Yamkela <yamkela.qhogwana@outlook.com>
2025-05-05 21:56:01 +0200

    changes

--- zeitara.domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ zeitara.domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0095b45b53107dde1272dd1b984e7269f2002848")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ad65804018dad07912516f8bc2080dd8ef8b37a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
